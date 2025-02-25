da25fc6d6eae216656e2369417e6bfae274fabc3
今天吃什么 <65970292+LingXilei10@users.noreply.github.com>
2025-02-25 13:51:22 +0800

    Update list.cpp

--- basic/list.cpp
+++ basic/list.cpp
@@ -1,4 +1,4 @@
-// initialization
+//==================initialization==================
 
 #include <list>
 
@@ -17,7 +17,7 @@ std::list<int> lst{1, 3, 5};
 std::list<int> lst(n, 2);
 
 
-// func
+//============================func===============================
 
 #include <iostream>
 #include <list>
@@ -69,3 +69,67 @@ int main() {
 
     return 0;
 }
+
+
+//===================origin===================
+struct ListNode{
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x),next(nullptr) {}
+};
+
+ListNode *dummynode = new ListNode(0);
+ListNode *cur = dummynode;
+int val;
+cin>>val;
+for(int i=0;i<n;i++){
+    ListNode *newnode = new ListNode(val);
+    cur->next = newnode;
+    cur = cur->next;
+}
+
+void printnode(ListNode *head){
+    ListNode *cur =head;
+    while(cur->next!=NULL){
+        cout<<cur->next->val<<" ";
+        cur = cur->next;
+    }
+}
+
+
+
+//===================create===================
+ListNode *createLinkedList(vector<int> arr){
+    if(arr.empty()){
+        return nullptr;   //  head = nullptr;
+    }
+    else{
+        ListNode *head = new ListNode(arr[0]);
+        ListNode *cur = head;
+        for(int i=1; i<arr.size(); i++){
+            cur->next = new ListNode(arr[i]);
+            cur = cur->next;
+        }
+        return head;
+    }
+}
+//=================遍历=====================
+for(ListNode *p=head;p!=nullptr; p=p->next){
+    cout<<p->val;
+}
+//====={1,2,3,4,5}========
+//===================增删=====================
+//在第3个后、第4个之前加一个 指向第3个 
+head = createLinkedList({1,2,3,4,5});
+ListNode *cur = head;
+for(int i=0;i<2;i++){
+    cur = cur->next;
+}
+ListNode *newnode = new ListNode(0);
+newnode->next = cur->next;
+cur->next = newnode;
+
+//删去第4个  还是指向第3个
+
+cur->next = cur->next->next;
+
